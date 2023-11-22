85e30106b433610651e5072cbefcc1b9ce00b174
soohyuneeee <202110214@bssm.hs.kr>
2023-11-22 11:38:11 +0900

    {Add} init

---
+++ src/main/java/service/server/chewing/ChewingApplication.java
@@ -0,0 +1,13 @@
+package service.server.chewing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChewingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChewingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/service/server/chewing/ChewingApplicationTests.java
@@ -0,0 +1,13 @@
+package service.server.chewing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChewingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
